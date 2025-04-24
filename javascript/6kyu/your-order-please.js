61c00811f2e1f5af92bfc190ed2f0ddc797e6a4f
Kamata954 <kimani.walker954@gmail.com>
2025-04-24 15:54:19 -0500

    daily coding practice

---
+++ javascript/6kyu/your-order-please.js
@@ -0,0 +1,30 @@
+function order(words){
+   
+    let wordsArr = words.split(" ");
+     
+     function getNumber (word) {
+       return word.split("").filter(letter => Number.isInteger(+letter) );
+     }
+     
+     return wordsArr.sort((a,b) => getNumber(a) - getNumber(b)).join(" ");
+     
+   }
+   
+   
+   //split words into array of words
+   //split each word into letters
+   //create a function that takes in a word and returns the integer
+   //sort, put word into function - function
+
+//INSTRUCTIONS
+// Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+// If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+// Examples
+
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+// "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+// ""  -->  ""
+
