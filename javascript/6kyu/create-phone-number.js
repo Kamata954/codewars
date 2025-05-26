0c41e20b6d0a8872928778a76d05bf353e1c6dca
Kamata954 <kimani.walker954@gmail.com>
2025-05-26 00:54:18 -0400

    daily coding challenge

---
+++ javascript/6kyu/create-phone-number.js
@@ -0,0 +1,19 @@
+function createPhoneNumber(numbers){
+  return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`
+}
+
+// Ideal solution is to use replace, make a string like (xxx) xxx-xxxx
+//loop over length of string
+//replace x with string[i], make sure you make the string equal to the replaced value since
+//replace doesnt mutatte the original string but just returns a new one
+//pretty sure replace will move from left to right
+
+
+//Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+// Example
+
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+
+// The returned format must be correct in order to complete this challenge.
+
+// Don't forget the space after the closing parentheses!
